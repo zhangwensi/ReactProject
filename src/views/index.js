c608bbc27148ca9b04aec610004163a11a0ee590
Your Name <you@example.com>
2020-04-18 10:46:50 +0800

    路由懒加载

--- src/views/index.js
+++ src/views/index.js
@@ -1,8 +1,35 @@
-import Aritcle  from './Aritcle'
-import Dashbroad from './Dashborad'
-import Login from './Login'
-import NotFond from './NotFond'
-import Edit from './Aritcle/Edit'
+// import Aritcle  from './Aritcle'
+// import Dashbroad from './Dashborad'
+// import Login from './Login'
+// import NotFond from './NotFond'
+// import Edit from './Aritcle/Edit'
+import Loadable  from 'react-loadable'
+import Loading from '../components/loading'
+
+const Aritcle = Loadable({
+    loader:()=> import('./Aritcle'),
+    loading: Loading
+})
+
+const Dashbroad = Loadable({
+    loader:()=> import('./Dashborad'),
+    loading: Loading
+})
+
+const Login = Loadable({
+    loader:()=> import('./Login'),
+    loading: Loading
+})
+
+const NotFond = Loadable({
+    loader:()=> import('./NotFond'),
+    loading: Loading
+})
+
+const Edit = Loadable({
+    loader:()=> import('./Aritcle/Edit'),
+    loading: Loading
+})
 
 
 export {
