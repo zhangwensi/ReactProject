47fafbf38e1fa824ee156650ff96c3a39e6add3c
Your Name <you@example.com>
2020-04-19 20:42:14 +0800

    渲染页面

--- src/views/Aritcle/index.js
+++ src/views/Aritcle/index.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react'
 
-import { Card, Button, Table } from 'antd'
+import { Card, Button, Table,Tag } from 'antd'
+
+import moment from 'moment'
 
 import { getList } from '../../service'
+
+const columnsTypeMap ={
+    id:'序号',
+    title: '标题',
+    author: '作者',
+    amount: '阅读量',
+    createAt: '创建时间',
+    action: '操作'
+}
 
 export default class Aritcle extends Component {
     constructor(){
@@ -49,20 +60,69 @@ export default class Aritcle extends Component {
             total: 0
         }               
     }
-    UNSAFE_componentWillMount(){
-        getList().then(resp=>{
-            this.setState.total = resp.total
+    columnsType = (key) =>{
+        const culomes =  key.map(item=>{
+            if(item === 'amount') {
+                return {
+                    title: columnsTypeMap[item],
+                    dataIndex: item,
+                    key: item,
+                    render:(record)=>{
+                        return <Tag color={record<70 ? 'red':'green'}>{record}</Tag>
+                    }
+                }
+            }
+            if(item === 'createAt') {
+                    return {
+                        title: columnsTypeMap[item],
+                        dataIndex: item,
+                        key: item,
+                        render:(record)=>{
+                            return moment(record).format("YYYY年MM月DD日 HH点mm分ss秒")
+                    }
+                }
+            }
+            return {
+                title: columnsTypeMap[item],
+                dataIndex: item,
+                key: item
+            }
         })
+        culomes.push({
+            title: '操作',
+            dataIndex: 'action',
+            key: 'action',
+            render:(record)=>{
+            return <Button type="primary">编辑</Button>
+            }
+        })
+        return culomes
+    }
+    getData = () =>{
+        getList().then(resp=>{
+            const columnsKey = Object.keys(resp.data.list[0])
+            const columns = this.columnsType(columnsKey)
+            this.setState({
+                columns,
+                total:resp.data.total,
+                data:resp.data.list
+            })
+        })
+    }
+    UNSAFE_componentWillMount(){
+        this.getData()
     }
     render() {
         return (
             <>
-                <Card title="文章列表" bordered={false} extra={<Button type="primary">更多</Button>}>
+                <Card title="文章列表" bordered={false} extra={<Button type="default">导出exel</Button>}>
                     <Table
-                        rowKey='id'
+                        rowKey={record => record.id}
                         columns={this.state.columns}
                         dataSource={this.state.data}
-                        pagination={{total:this.state.total}}
+                        pagination={{
+                            total:this.state.total
+                        }}
                     />
                 </Card>
 
