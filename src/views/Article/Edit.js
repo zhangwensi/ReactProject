f9afe2ce3260773032bc41523d639a8c5bdea6ae
Your Name <you@example.com>
2020-06-12 00:30:09 +0800

    初始化eacherts图表页并创建第一个实例

--- src/views/Article/Edit.js
+++ src/views/Article/Edit.js
@@ -1,19 +1,145 @@
-import React, { Component } from 'react'
-import { Card, Button } from 'antd'
+import React, { Component,createRef } from 'react'
+import { Card, Button,Form, Input,DatePicker } from 'antd'
+import E from 'wangeditor'
+const formItemLayout = {
+    labelCol: {
+      span: 4,
+    },
+    wrapperCol: {
+      span: 8,
+    },
+  }
 
-export default class Edit extends Component {
+const  formItemLayout2 = {
+    labelCol: {
+        span: 4,
+      },
+      wrapperCol: {
+        span:12,
+      }
+}
+const tailLayout = {
+    wrapperCol: {
+        offset: 4,
+        span: 4,
+    }
+}
 
+const onFinish = (values ) => {
+    console.log(values)
+}
+
+const onFinishFailed = (values ) => {
+    console.log(values)
+}
+
+class Edit extends Component {
+    constructor(){
+        super()
+        // 因富文本编辑器需要操作DOM,而React中进行DOM操作的需要使用到Ref,在div中使用ref属性进行DOM绑定,估要引入createRef
+        this.editeRef = createRef()
+    }
     goback = () =>{
         this.props.history.push('/admin/article')
     }
+    // 初始化富文本编辑器
+    initEditer = () => {
+        // this.editeRef.current才是最终的DOM  setFieldsValue
+        this.editor = new E(this.editeRef.current)
+        this.editor.customConfig.onchange = (html) => {
+            // html 即变化之后的内容
+            console.log(html)
+            console.log(this.form)
+            // this.setFieldsValue({
+            //     content:html
+            // })
+        }
+        this.editor.create()
+    }
 
+    componentDidMount(){
+        this.initEditer()
+    }
     render() {
         return (
             <>
                 <Card title="文章编辑" bordered={false} extra={<Button onClick={this.goback}>返回</Button>}>
-
+                    <Form 
+                        {...formItemLayout}
+                        onFinish={onFinish}
+                        onFinishFailed={onFinishFailed}
+                    >
+                        <Form.Item 
+                            label="文章标题" 
+                            name = "title"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: '文章标题不为空',
+                                }  
+                            ]}
+                        >
+                            <Input placeholder="请输入文章标题" />
+                        </Form.Item>
+                        <Form.Item
+                            label="作者" 
+                            name = "author"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: '作者不为空',
+                                }  
+                            ]}
+                        >
+                            <Input placeholder="请输入文章作者" />
+                        </Form.Item>
+                        <Form.Item
+                            label="阅读量" 
+                            name = "amount"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: '阅读量不为空',
+                                }  
+                            ]}
+                        >
+                            <Input placeholder="请输入文章阅读量" />
+                        </Form.Item>
+                        <Form.Item
+                            label="创建时间" 
+                            name = "creatAt"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: '创建时间不为空',
+                                }  
+                            ]}
+                        >
+                            <DatePicker showTime/>
+                        </Form.Item>
+                        <Form.Item
+                            {...formItemLayout2}
+                            label="文章内容"
+                            name = "contnet"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: '内容不为空',
+                                }  
+                            ]}
+                        >
+                            <div ref={this.editeRef} />
+                        </Form.Item>
+                        <Form.Item {...tailLayout}>
+                            <Button type="primary" htmlType="submit">
+                                保存修改
+                            </Button>
+                        </Form.Item>
+                    </Form>
                 </Card>
             </>
         )
     }
 }
+
+export default Edit
--- src/views/Dashbord/index.js
+++ src/views/Dashbord/index.js
@@ -1,11 +1,91 @@
-import React, { Component } from 'react'
+import React, { Component,createRef } from 'react'
+
+import {Card,Row, Col } from 'antd'
+
+import tabEcharts from 'echarts'
+
+import './dashbord.less'
 
 export default class Dashbord extends Component {
+    constructor(){
+        super()
+        this.showEacherts = createRef()
+        this.showEacherts1 = createRef()
+    }
+    // 初始化图表
+    initEacherts = () =>{
+        this.myChart = tabEcharts.init(this.showEacherts.current)
+        // 指定图表的配置项和数据
+        const option = {
+            title: {
+                text: '浏览量'
+            },
+            tooltip: {},
+            legend: {
+                data:['销量']
+            },
+            xAxis: {
+                data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+            },
+            yAxis: {},
+            series: [{
+                name: '销量',
+                type: 'bar',
+                data: [5, 20, 36, 10, 10, 20]
+            }]
+        }// 使用刚指定的配置项和数据显示图表。
+        this.myChart.setOption(option);
+    }
+
+    initEacherts1 = () =>{
+        this.myChart = tabEcharts.init(this.showEacherts1.current)
+        
+        this.myChart.setOption({
+            series : [
+                {
+                    name: '访问来源',
+                    type: 'pie',
+                    radius: '55%',
+                    data:[
+                        {value:235, name:'视频广告'},
+                        {value:274, name:'联盟广告'},
+                        {value:310, name:'邮件营销'},
+                        {value:335, name:'直接访问'},
+                        {value:400, name:'搜索引擎'}
+                    ]
+                }
+            ]
+        })
+    }
+
+    componentDidMount() {
+        this.initEacherts()
+        this.initEacherts1()
+    }
     render() {
         return (
-            <div>
-                图形列表
-            </div>
+            <>
+                <Card title="控制图" bordered={false}>
+                    <Row gutter={4}>
+                        <Col span={6}>
+                            <Card.Grid className="sm-ecarths">展示区1</Card.Grid>
+                        </Col>
+                        <Col span={6}>
+                            <Card.Grid className="sm-ecarths">展示区2</Card.Grid>
+                        </Col>
+                        <Col span={6}>
+                            <Card.Grid className="sm-ecarths">展示区3</Card.Grid>
+                        </Col>
+                        <Col span={6}>
+                            <Card.Grid className="sm-ecarths">展示区3</Card.Grid>
+                        </Col>
+                    </Row>
+                </Card>
+                <Card className="sj-echarts" title="图表" bordered={false}>
+                    <div className="tb-echarts" ref={this.showEacherts}></div>
+                    <div className="tb-echarts" ref={this.showEacherts1}></div>
+                </Card>
+            </>
         )
     }
 }
